3e1d94b7f25316976d835b45c21582fa6a307b79
nelsonbuhler <122967704+Nellie-Buhler@users.noreply.github.com>
2024-09-24 17:07:13 +0200

    finis

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,8 @@ gsap.registerPlugin(ScrollTrigger);
     Déplacer l'item 1 de 100px vers la gauche
     durant 3 secondes
    ----------------------- */
+const jsExercise1 = document.querySelector("#js-exercise-1");
+gsap.to(jsExercise1, { x: "100px", duration: 3 });
 
 /* -----------------------
     Exercice 2
@@ -21,7 +23,8 @@ gsap.registerPlugin(ScrollTrigger);
     durant 2 secondes
     après un délai de 2 secondes
    ----------------------- */
-
+const jsExercise2 = document.querySelector("#js-exercise-2");
+gsap.to(jsExercise2, { rotate: 45, duration: 2, delay: 2 });
 /* -----------------------
     Exercice 3
    -----------------------
@@ -30,7 +33,8 @@ gsap.registerPlugin(ScrollTrigger);
     durant 2 secondes
     après un délai de 1 secondes
    ----------------------- */
-
+const jsExercise3 = document.querySelector("#js-exercise-3");
+gsap.to(jsExercise3, { scale: 0.75, opacity: 0.5, duration: 2, delay: 1 });
 /* -----------------------
     Exercice 4
    -----------------------
@@ -39,7 +43,8 @@ gsap.registerPlugin(ScrollTrigger);
     durant 2 secondes
     après un délai de 1 secondes
    ----------------------- */
-
+const jsExercise4 = document.querySelector("#js-exercise-4");
+gsap.from(jsExercise4, { scale: 0.75, opacity: 0.5, duration: 2, delay: 1 });
 /* -----------------------
     Exercice 5 (timeline)
    -----------------------
@@ -50,7 +55,11 @@ gsap.registerPlugin(ScrollTrigger);
     PUIS déplacer l'item 5 de 100px vers le haut
     durant 2 secondes après un délai de 1 seconde
    ----------------------- */
-
+const jsExercise5 = document.querySelector("#js-exercise-5");
+const tl = gsap.timeline();
+tl.to(jsExercise5, { x: -100, duration: 2 });
+tl.to(jsExercise5, { rotate: 45, duration: 2, delay: 1 });
+tl.to(jsExercise5, { y: -100, duration: 2, delay: 1 });
 /* -----------------------
     Exercice 6 (timeline)
    -----------------------
@@ -59,7 +68,9 @@ gsap.registerPlugin(ScrollTrigger);
     ET SIMULTANEMENT changer l'item 6 d'échelle (75%)
     durant 5 secondes
    ----------------------- */
-
+const jsExercise6 = document.querySelector("#js-exercise-6");
+tl.to(jsExercise6, { y: 100, duration: 3 }, 0);
+tl.to(jsExercise6, { scale: 0.75, duration: 5 }, 0);
 /* -----------------------
     Exercice 7 (repeat + yoyo)
    -----------------------
@@ -68,10 +79,25 @@ gsap.registerPlugin(ScrollTrigger);
     avec un easing elastic.out
     et répéter ce mouvement à l'infini
    ----------------------- */
-
+const jsExercise7 = document.querySelector("#js-exercise-7");
+tl.to(
+  jsExercise7,
+  { rotate: 135, duration: 2, ease: "elastic.out", repeat: -1 },
+  0
+);
 /* -----------------------
     Exercice 8
    -----------------------
     Réaliser une animation libre
     lorsque le bouton est cliqué
    ----------------------- */
+const jsExercise8 = document.querySelector("#js-exercise-8");
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  const tl = gsap.timeline();
+  tl.to(
+    jsExercise8,
+    { rotate: 135, duration: 2, ease: "elastic.out", yoyo: true },
+    0
+  );
+});
